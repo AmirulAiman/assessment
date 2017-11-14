dae7085743354bd7b3e03b3770f780ceff359ba5
AmirulAiman <aiman_amirul@ymail.com>
2017-11-14 11:25:20 +0800

    create database column and seeder

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Questions;
 class QuestionTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,52 @@ class QuestionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Question 1
+        $question1 = new Questions();
+        $question1->questions = 'Yang manakah berikutnya merupakan tumbuhan yang tidak berbunga?';
+        $question1->CollAnswers = serialize(array(
+            array('A','Pokok jagung'), 
+            array('B','Pokok bunga raya'), 
+            array('C','Pokok bunga ros'), 
+            array('D','Pokok paku pakis')
+        ));
+        $question1->answer = 'A';
+        $question1->save();
+
+        //Question2
+        $question2 = new Questions();
+        $question2->questions = 'Plih perkataan sinonim bagi <u><b>berazam</b></u>';
+        $question2->CollAnswers = serialize(array(
+            array('A','bersatu'), 
+            array('B','berpadu'), 
+            array('C','bertekad'), 
+            array('D','bersepakat')
+        ));
+        $question2->answer = 'C';
+        $question2->save();
+        
+        //Question3
+        $question3 = new Questions();
+        $question3->questions = 'Pilih perkataan yang antonim bagi <u><b>pemurah</b></u>';
+        $question3->CollAnswers = serialize(array(
+            array('A','dermawan'), 
+            array('B','pemboros'), 
+            array('C','peramah'), 
+            array('D','kedekut')
+        ));
+        $question3->answer = 'D';
+        $question3->save();
+        
+        //Question4
+        $question4 = new Questions();
+        $question4->questions = 'Pilih ayat yang <u><b>betuk</b></u>';
+        $question4->CollAnswers = serialize(array(
+            array('A','Lelaki yang segak itu ayah saya.'), 
+            array('B','Kedua-dua lelaki itu adalah adik-beradik.'), 
+            array('C','Rumah yang baru dibeli oleh ayah adalah terlalu besar.'), 
+            array('D','Segala pengakuaan yang dibuat didepan hakim adalah benar.')
+        ));
+        $question4->answer = 'B';
+        $question4->save();
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class UserTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,17 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user1 = new User();
+        $user1->username = 'Admin1';
+        $user1->password = bcrypt('abc123');
+        $user1->save();
+        $user2 = new User();
+        $user2->username = 'Admin2';
+        $user2->password = bcrypt('abc123');
+        $user2->save();
+        $user3 = new User();
+        $user3->username = 'Admin3';
+        $user3->password = bcrypt('abc123');
+        $user3->save();
     }
 }
