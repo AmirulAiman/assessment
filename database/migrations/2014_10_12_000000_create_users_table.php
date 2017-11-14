094566ed03ff19c4c6c1268f64ebd420f6b37555
AmirulAiman <aiman_amirul@ymail.com>
2017-11-14 12:00:01 +0800

    Solve string access violation, add user_type in the User table and UserTableSeeder data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('username')->unique();
             $table->string('password');
+            $table->integer('user_type');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,17 +11,32 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        //Admin 1
         $user1 = new User();
         $user1->username = 'Admin1';
         $user1->password = bcrypt('abc123');
+        $user1->user_type = 0;
         $user1->save();
+        
+        //Student 1
         $user2 = new User();
-        $user2->username = 'Admin2';
+        $user2->username = 'Std1';
         $user2->password = bcrypt('abc123');
+        $user2->user_type = 1;
         $user2->save();
+        
+        //Student 2
         $user3 = new User();
-        $user3->username = 'Admin3';
+        $user3->username = 'Std2';
         $user3->password = bcrypt('abc123');
+        $user3->user_type = 1;
         $user3->save();
+        
+        //Student 3
+        $user4 = new User();
+        $user4->username = 'Std3';
+        $user4->password = bcrypt('abc123');
+        $user4->user_type = 1;
+        $user4->save();
     }
 }
